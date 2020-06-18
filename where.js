6e9cc0822ce7e77720a013e97c3dd1e867f75aca
Visi@n <broi.antonio@gmail.com>
2020-06-18 21:57:27 +0200

    Add files via upload

---
+++ where.js
@@ -0,0 +1,9 @@
+myData = [
+[32.8896686,-96.9679177, '1231 Greenway Dr # 600, Irving, TX 75038, USA'],
+[32.7895375,-96.81962779999999, '1318 N Riverfront Blvd, Dallas, TX 75207, USA'],
+[33.0173417,-96.8352462, '6300 International Pkwy, Plano, TX 75093, USA'],
+[32.9454674,-96.73765879999999, '601 S Central Expy, Richardson, TX 75080, USA'],
+[44.51302099999999,11.2698734, 'Via Antonio Cavalieri Ducati, 74/5, 40132 Bologna BO, Italy'],
+[44.6520833,10.9365944, 'Via Paolo Ferrari, 85, 41121 Modena MO, Italy'],
+[44.7315319,11.2908156, '44042 Cento, Province of Ferrara, Italy']
+];
